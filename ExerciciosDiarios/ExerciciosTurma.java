b4aba2929cb600e2ba210ca3ffb0e17879bf8698
filipecunha11 <f.cunha@datagen.eu>
2016-11-09 11:41:46 +0000

    Update ExerciciosTurma.java

--- ExerciciosDiarios/ExerciciosTurma.java
+++ ExerciciosDiarios/ExerciciosTurma.java
@@ -16,8 +16,8 @@ public class ExerciciosTurma {
 	public void exercicio1()
 	{
 		/* EXERCICIO 1  - Dificuldade I
-		 * Peça 5 valores ao utilizador entre 1 e 100 e guarde-os num array
-		 * Através de um ciclo, determine qual é o maior valor e o menor
+		 * PeÃ§a 5 valores ao utilizador entre 1 e 100 e guarde-os num array
+		 * AtravÃ©s de um ciclo, determine qual Ã© o maior valor e o menor
 		 * 
 		 */
 		int[] nums = new int [5]; int max=0, min=101;
@@ -36,16 +36,16 @@ public class ExerciciosTurma {
 			if (nums[i] < min)
 				min = nums[i];
 		}
-		System.out.println("O numero maior é: " + max);
-		System.out.println("O numero menor é: " + min);
+		System.out.println("O numero maior Ã©: " + max);
+		System.out.println("O numero menor Ã©: " + min);
 	}
 	
 	public void exercicio2()
 	{
 		/* EXERCICIO 2  - Dificuldade I
-		 * Peça 5 valores ao utilizador, em que 1 desses valores tem de corresponder a 0, e guarde-os num array.
+		 * PeÃ§a 5 valores ao utilizador, em que 1 desses valores tem de corresponder a 0, e guarde-os num array.
 		 * De seguida, percorra o array e descubra em que casa foi guardado o valor 0 e retorne ao utilizador, tendo
-		 *  em conta que array[0] corresponde à primeira casa.
+		 *  em conta que array[0] corresponde Ã  primeira casa.
 		 */
 		 
 		int[] nums = new int [5];
@@ -56,24 +56,24 @@ public class ExerciciosTurma {
 		
 		for (int i = 0; i < 5; i++)
 			if (nums[i] == 0)
-				System.out.println("O 0 foi inserido na " + i+1 + "ª casa.");
+				System.out.println("O 0 foi inserido na " + i+1 + "Âª casa.");
 		
 	}
 	
 	public void exercicio3()
 	{
 		/* EXERCICIO 3 - Dificuldade III
-		 * Crie um array com 50 espaços e preencha-o com os números de 1 a 50
-		 * De seguida, gere 3 números aleatórios que irão corresponder às casas do array com premio
-		 * Pergunte ao utilizador quanto € pretende apostar
-		 * Peça três valores ao utilizador entre 1-50 e verifique se corresponde aos valores das casas do
-		 * array que contêm prémio.
-		 * Consoante o número de valores acertados, calcule o valor do prémio do utilizador. O premio é 
+		 * Crie um array com 50 espaÃ§os e preencha-o com os nÃºmeros de 1 a 50
+		 * De seguida, gere 3 nÃºmeros aleatÃ³rios que irÃ£o corresponder Ã s casas do array com premio
+		 * Pergunte ao utilizador quanto â‚¬ pretende apostar
+		 * PeÃ§a trÃªs valores ao utilizador entre 1-50 e verifique se corresponde aos valores das casas do
+		 * array que contÃªm prÃ©mio.
+		 * Consoante o nÃºmero de valores acertados, calcule o valor do prÃ©mio do utilizador. O premio Ã© 
 		 * calculado da seguinte forma: ((valorApostado^2)^2)^2. Cada expoente corresponde a 1 valor acertado
 		 * Utilize 3 arrays, um para os valores inseridos pelo utilizador, outra para os numeros da lotaria e outro para os nums gerados
 		 * 
-		 * É recomendado utilizar o tipo DOUBLE para a variavel que guarda o valor ganho pelo utilizador
-		 * Para calcular exponenciais utilize a função: Math.pow(base,exp)
+		 * Ã‰ recomendado utilizar o tipo DOUBLE para a variavel que guarda o valor ganho pelo utilizador
+		 * Para calcular exponenciais utilize a funÃ§Ã£o: Math.pow(base,exp)
 		 */
 		final int VALORMAX = 49;
 		Random r = new Random();
@@ -89,18 +89,27 @@ public class ExerciciosTurma {
 			numLotaria[i] = i+1;
 		
 		
-		//Apenas para saber quais são os valores da lotaria
+		//Apenas para saber quais sÃ£o os valores da lotaria
 		System.out.println(numLotaria[valorRandom[0]] + ", " + numLotaria[valorRandom[1]] + ", " + numLotaria[valorRandom[2]]);
 		
-		System.out.print("Quanto pretende apostar?  ");
+		do
+		{
+		System.out.println("Quanto pretende apostar? MÃ­nimo 2â‚¬  ");
 		valorApostado = (new Scanner(System.in)).nextInt();
+		} while (valorApostado < 2);
 		
 		System.out.println("");
-		System.out.println("Apostou " + valorApostado + "€, poderá ganhar um total de: " + Math.pow(valorApostado,8));
+		System.out.println("Apostou " + valorApostado + "â‚¬, poderÃ¡ ganhar um total de: " + Math.pow(valorApostado,8));
 		for (int i = 0; i < 3; i++)
 		{
-			System.out.print(i+1 + "º numero:  ");
+			validado = false;
+			do
+			{
+			System.out.print(i+1 + "Âº numero:  ");
 			valorUser[i] = (new Scanner(System.in)).nextInt();
+			if (valorUser[i] > 0 && valorUser[i] < 51)
+				validado = true;
+			} while(!validado);
 		}
 		
 		
@@ -119,7 +128,7 @@ public class ExerciciosTurma {
 		
 		if (nAcertados == 0)
 		{
-			System.out.println("Não acertou nada, inseriu " + valorUser[0] + ", " + valorUser[1] + ", " + valorUser[2]+ "... ");
+			System.out.println("NÃ£o acertou nada, inseriu " + valorUser[0] + ", " + valorUser[1] + ", " + valorUser[2]+ "... ");
 			valorApostado = 0;
 		}
 		else if(nAcertados == 1)
@@ -130,6 +139,6 @@ public class ExerciciosTurma {
 			System.out.println("ACERTOU TODOS OS VALORES! Inseriu " + valorUser[0] + ", " + valorUser[1] + ", " + valorUser[2]+ ".");
 		System.out.println("Os valores da lotaria eram " + numLotaria[valorRandom[0]] + ", " + numLotaria[valorRandom[1]] + ", " + numLotaria[valorRandom[2]]+ ".");
 
-		System.out.println("Ganhou um total de " + valorApostado + "€.");
+		System.out.println("Ganhou um total de " + valorApostado + "â‚¬.");
 	}
 }
